59b50d1a74bdf7b56fe351a2dcca7b9d9d0aee22
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2022-08-25 22:22:37 +0200

    Migrate to craco

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
+/* eslint-disable global-require */
 // eslint-disable-next-line filenames/match-regex
 module.exports = {
 	plugins: [
-		// eslint-disable-next-line global-require
 		require("autoprefixer"),
-		// eslint-disable-next-line global-require
 		require("postcss-flexbugs-fixes"),
-		// eslint-disable-next-line global-require
 		require("postcss-preset-env")({
 			features: {
 				"nesting-rules": true
